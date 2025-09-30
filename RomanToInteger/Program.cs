e8847a959d389e1eb0b8962790cd7eab2c98c77f
ibnLotfi <ibnlotfi@hotmail.fr>
2025-09-30 13:02:34 +0200

    Initial Push, creating the repo

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Console App - Read Roman number notation and get the Integer.");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Write the roman number you want to convert. Allowed char 'I', 'V', 'X', 'L', 'C', 'D', 'M'. Maj only.");
+Console.ReadLine();
+
+
+
